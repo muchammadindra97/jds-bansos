18591bbf3d57a1a36a480bec9d2c1d06eda3b7e9
Muchammad Indra <muchammadindra97@gmail.com>
2022-09-15 20:51:38 +0700

    feat: change app title
    Change app title to "JDS - Bansos Covid-19".

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = "JDS - Bansos Covid-19";
+        return args;
+      })
+  }
 })
